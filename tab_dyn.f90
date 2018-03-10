62713d631dac3ae0bc72e28ec0b2f77503a8dca0
pelin sekercioglu <pelinsekercioglu@MacBook-Pro-de-pelin.local>
2018-03-10 01:53:03 +0100

    Bug fix

--- tab_dyn.f90
+++ tab_dyn.f90
@@ -1,7 +1,7 @@
 PROGRAM tab_dyn
 	IMPLICIT NONE 
 	
-	INTEGER :: n, imax, ok, i
+	INTEGER :: n, maxi, ok, i
 	REAL, DIMENSION(:), ALLOCATABLE :: tab
 	
 	PRINT*, "Entrez le nombre d'element de votre tableau"
@@ -13,12 +13,12 @@ PROGRAM tab_dyn
 		PRINT*, "Entrez l'element", i, "de votre tableau:" 
 		READ*, tab(i)
 	END DO
-	imax=tab(i)
+	maxi=tab(i)
 
 	DO i=2,n
-		IF(tab(i)>tab(imax))THEN
-			imax=tab(i)
+		IF(tab(i)>maxi)THEN
+			maxi=tab(i)
 		END IF
 	END DO
-	PRINT*, "Valeur max du tableau est", tab(imax)
+	PRINT*, "Valeur max du tableau est", maxi
 END PROGRAM tab_dyn
