b2ca1bba9f1c270b28cbcff68d020ed33aa51a7d
pelin sekercioglu <pelinsekercioglu@MacBook-Pro-de-pelin.local>
2018-03-10 01:52:26 +0100

    Bug fix

--- tab_dyn.f90
+++ tab_dyn.f90
@@ -7,20 +7,17 @@ PROGRAM tab_dyn
 	PRINT*, "Entrez le nombre d'element de votre tableau"
 	READ*, n
 	ALLOCATE(tab(n), STAT=ok)
-	IF (ok/=0)THEN
-		PRINT*, "Echec d'allocation memoire"
-		STOP
-	END IF
-	
+	IF (ok/=0)STOP "Echec d'allocation memoire"
+		
 	DO i=1,n
 		PRINT*, "Entrez l'element", i, "de votre tableau:" 
 		READ*, tab(i)
 	END DO
-	imax=i
+	imax=tab(i)
 
 	DO i=2,n
 		IF(tab(i)>tab(imax))THEN
-			imax=i
+			imax=tab(i)
 		END IF
 	END DO
 	PRINT*, "Valeur max du tableau est", tab(imax)
